3fcfdea48f10fff857a0efd4f4a9217e562802ca
pushpa <kokanepushpa06@gmail.com>
2025-12-22 15:09:15 +0530

    edit and add card funtionlity

--- main.js
+++ main.js
@@ -1,34 +1,85 @@
-const statusColumn = [
-    { id: 1, columnStatus: "To Do", totalCard: 2 },
+let trelloCardDetails = [
+    {
+        id: 1, columnStatus: "To Do", totalCard: 2,
+        tasks: [
+            {
+                id: 1,
+                title: 'functionality one',
+                descriptin: 'Test'
+            }
+        ]
+    },
     { id: 2, columnStatus: "In-Progress", totalCard: 0 },
     { id: 3, columnStatus: "Done", totalCard: 0 }
 ];
 
 let draggedCard = null;
+let activeColumnId = null;
 
+const updateTrelloDetailsObj = (taskDetails) => {
+    trelloCardDetails = trelloCardDetails.map(col => {
+        if (col.id === activeColumnId) {
+            return {
+                ...col,
+                tasks: [...(col.tasks || []), taskDetails],
+                totalCard: (col.totalCard || 0) + 1
+            };
+        }
+        return col;
+    });
+}
 window.onload = function () {
 
-    const addCardToColumn = (colId) => {
-        let cardContainer = document.getElementById('listCardContent' + colId);
+    document.getElementById('saveCard').addEventListener('click', () => {
+        let cardContainer = document.getElementById('listCardContent' + activeColumnId);
         let cardCnt = cardContainer.getElementsByClassName('card-data').length;
+        let taskName = document.getElementById('card_title').value;
+        let taskDesc = document.getElementById('card_desc').value;
+        let taskDetails = { id: cardCnt, title: taskName, descriptin: taskDesc }
+        updateTrelloDetailsObj(taskDetails);
+        card = createCard(taskDetails, activeColumnId)
+        cardContainer.appendChild(card);
+        cardContainer.scrollTop = cardContainer.scrollHeight;
+        document.getElementById('card_title').value='';
+        document.getElementById('card_desc').value='';
+        document.getElementById('card_modal').classList.add('hide');
+    });
 
+    const createCard = (t, colId) => {
         let card = document.createElement('div');
-        card.textContent = `Card ${cardCnt + 1}`;
+        let cardTextContent = `<div class='card-details'><div class='card-title'>${t.title}</div><div class='card-description'>${t.descriptin}</div></div><button class='task-edit'>...</button>`
+        card.dataset.taskId = t.id;
+        card.dataset.columnId = colId;
+        card.innerHTML = cardTextContent;
         card.classList.add('card-data');
         card.draggable = true;
 
         card.addEventListener('dragstart', dragStartHandler);
         card.addEventListener('dragend', dragEndHandler);
+        card.querySelector('.task-edit').addEventListener('click',()=>{
+            console.log(t.id,colId)
+            activeColumnId=colId;
+            document.getElementById('card_title').value = t.title;
+            document.getElementById('card_desc').value = t.descriptin;
+            document.getElementById('card_modal').classList.remove('hide');
 
-        cardContainer.appendChild(card);
-        cardContainer.scrollTop = cardContainer.scrollHeight;
+        })
+        return card;
+    }
+    const addCardToColumn = (colId) => {
+        activeColumnId = colId;
+        document.getElementById('card_modal').classList.remove('hide');
     };
 
-    const dragStartHandler = (e) => {
-        draggedCard = e.target;
-        e.dataTransfer.effectAllowed = "move";
-        e.target.classList.add('dragging');
-    };
+ const dragStartHandler = (e) => {
+    draggedCard = e.target;
+
+    draggedCard.dataset.sourceColumnId =
+        draggedCard.dataset.columnId;
+
+    e.dataTransfer.effectAllowed = "move";
+    e.target.classList.add('dragging');
+};
 
     const dragEndHandler = (e) => {
         e.target.classList.remove('dragging');
@@ -51,6 +102,54 @@ window.onload = function () {
             }
         }, { offset: Number.NEGATIVE_INFINITY }).element;
     };
+
+    
+    const dropHandler = (e) => {
+    e.preventDefault();
+    const targetColumnId = Number(
+        e.currentTarget.id.replace('listCardContent', '')
+    );
+    if (!draggedCard) return;
+    if (sourceColumnId === targetColumnId) return;
+
+    const taskId = Number(draggedCard.dataset.taskId);
+    const sourceColumnId = Number(draggedCard.dataset.columnId);
+    updateTrelloOnDrag(taskId, sourceColumnId, targetColumnId);
+    draggedCard.dataset.columnId = targetColumnId;
+};
+
+    const updateTrelloOnDrag = (taskId, fromColId, toColId) => {
+    let movedTask = null;
+    trelloCardDetails = trelloCardDetails.map(col => {
+        if (col.id === fromColId) {
+            const remainTasks = (col.tasks || []).filter(t => {
+                if (t.id === taskId) {
+                    movedTask = t;
+                    return false;
+                }
+                return true;
+            });
+
+            return {
+                ...col,
+                tasks: remainTasks,
+                totalCard: remainTasks.length
+            };
+        }
+
+        if (col.id === toColId) {
+            return {
+                ...col,
+                tasks: [...(col.tasks || []), movedTask],
+                totalCard: (col.totalCard || 0) + 1
+            };
+        }
+
+        return col;
+    });
+};
+
+
     const dragOverHandler = (e) => {
         e.preventDefault();
 
@@ -68,7 +167,7 @@ window.onload = function () {
 
     let columnContainer = document.getElementById("columnContainer");
 
-    statusColumn.map((obj) => {
+    trelloCardDetails.map((obj) => {
         let card = document.createElement('div');
         card.classList.add('card');
 
@@ -77,16 +176,22 @@ window.onload = function () {
                 <div class='card-header-text'>${obj.columnStatus}</div>
                 <div class='card-header-right'><button>...</button></div>
             </div>
-            <div class='card-content'
-                id='listCardContent${obj.id}'
-                ondragover="event.preventDefault()">
-            </div>
+            <div class='card-content' id='listCardContent${obj.id}'</div>
         `;
+
         card.innerHTML = cardHtml;
 
         let contentArea = card.querySelector('.card-content');
         contentArea.addEventListener('dragover', dragOverHandler);
+        contentArea.addEventListener('drop', dropHandler);
 
+
+        if (obj.tasks?.length) {
+            obj.tasks.forEach((t) => {
+                const taskCard = createCard(t, obj.id);
+                contentArea.appendChild(taskCard);
+            });
+        }
         contentArea.scrollTop = contentArea.scrollHeight;
         let addCard = document.createElement('button');
         addCard.textContent = '+ add a card';
@@ -97,3 +202,9 @@ window.onload = function () {
         columnContainer.appendChild(card);
     });
 };
+
+const closeModal = () => {
+    document.getElementById('card_modal').classList.add('hide');
+}
+
+console.log(JSON.stringify(trelloCardDetails))
